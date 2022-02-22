5fa6ede62b528797f71e48bae536fc0000ff33ac
miriam <m.moreno.clavijo@gmail.com>
2022-02-22 18:05:01 +0100

    creating app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PaisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
