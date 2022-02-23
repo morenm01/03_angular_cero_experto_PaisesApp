35a67b90d472fd2f4a071365a5d3393b31e5f453
miriam <m.moreno.clavijo@gmail.com>
2022-02-23 16:45:42 +0100

    Definimos un objeto Subject y nos subscribimos en el constructor, ademas creamos un evento en el formulario para ejecutar un método accediento al valor por el evento

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,17 +1,32 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-pais-input',
   templateUrl: './pais-input.component.html'
 })
-export class PaisInputComponent  {
+export class PaisInputComponent implements OnInit {
 
   constructor() { }
 
   @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();
   termino: string = '';
 
+  ngOnInit(){
+    this.debouncer.subscribe(valor => {
+      console.log('debouncer:', valor);
+    })
+  }
   buscar(){
     this.onEnter.emit(this.termino);
+  }
+
+  teclaPresionada(event: any){
+    const valor = event.target.value;
+    console.log(event);
+    console.log('valor', valor);
   }
 }
