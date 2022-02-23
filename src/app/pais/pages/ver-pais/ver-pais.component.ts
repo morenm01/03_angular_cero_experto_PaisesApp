53e38ac8ddef219f1fedf3d7de6482eace94fb18
miriam <m.moreno.clavijo@gmail.com>
2022-02-23 18:38:01 +0100

    usamos pipe y switchMap para contraer dos subcribers en uno enver Pais por Alpha

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    /*this.activatedRoute.params
+      .subscribe(({ id }) => {
+        this.paisService.getPaisPorAlpha(id)
+        .subscribe((pais) => {
+          console.log(pais);
+      });
+    });*/
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id))
+      )
+      .subscribe((pais) => {
+          console.log(pais);
+      });
+    }
 
 }
